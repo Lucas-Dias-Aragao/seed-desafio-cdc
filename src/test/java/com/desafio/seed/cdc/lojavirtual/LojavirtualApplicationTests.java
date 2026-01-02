addc1d88ee203c983997799eadbbc57eed5fa670
LucasDias <lucasd.aragao@hotmail.com>
2026-01-02 19:21:27 -0300

    commit inicial

---
+++ src/test/java/com/desafio/seed/cdc/lojavirtual/LojavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.seed.cdc.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
