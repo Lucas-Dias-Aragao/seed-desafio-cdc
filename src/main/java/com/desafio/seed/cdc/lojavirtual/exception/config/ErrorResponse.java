2fa7cd33345a51fc311121847a962ea2e7a014d3
LucasDias <lucasd.aragao@hotmail.com>
2026-01-11 19:14:27 -0300

    feat: adiciona nova tratativa de erros

--- src/main/java/com/desafio/seed/cdc/lojavirtual/exception/config/ErrorResponse.java
+++ src/main/java/com/desafio/seed/cdc/lojavirtual/exception/config/ErrorResponse.java
@@ -1,20 +1,32 @@
 package com.desafio.seed.cdc.lojavirtual.exception.config;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
     private HttpStatus status;
     private String message;
     private List<FieldErrors> fieldErrorsList;
+    private LocalDateTime timestamp;
 
+    public ErrorResponse(String message) {
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ErrorResponse(String message, HttpStatus status, List<FieldErrors> fieldErrorsList) {
+        this.message = message;
+        this.status = status;
+        this.fieldErrorsList = fieldErrorsList;
+    }
 }
